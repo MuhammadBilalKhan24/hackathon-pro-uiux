587b8b322ce3b7928d8079e50426a1e0a897e38c
Muhammad Bilal Khan <162311474+MuhammadBilalKhan24@users.noreply.github.com>
2024-12-09 00:57:57 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,11 @@ import SubHero from "./component/Subhero";
 import Slides from "./component/Slides";
 import Products from "./component/Products";
 import Footer from "./component/Footer";
-import Header from "./component/Header";
 
 export default function Home(){
   return (
     <>
-    {/* <Header/> */}
+  
     <Hero/>
     <SubHero/>
     <Products/>
