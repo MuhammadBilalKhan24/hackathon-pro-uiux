0dc7cc7e310974290a4f75d219265ed17d5a6474
Muhammad Bilal Khan <162311474+MuhammadBilalKhan24@users.noreply.github.com>
2024-12-12 23:15:53 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,21 @@ import Hero from "./component/Hero";
 import SubHero from "./component/Subhero";
 import Slides from "./component/Slides";
 import Products from "./component/Products";
-import Footer from "./component/Footer";
+import Footer from "./component/Footer"; 
+import React from "react";
+import Banner from "./component/Banner";
 
 export default function Home(){
   return (
     <>
-  
+  <div>
     <Hero/>
     <SubHero/>
     <Products/>
     <Slides/>
     <Footer/>
+    <Banner/>
+    </div>
     </>
   )
 }
